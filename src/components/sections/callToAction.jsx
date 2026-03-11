de1fef29b71ead20a700ebf8ca70aef63c76fc12
Al Zayan P <alzayanpoothakkal1156@gmail.com>
2026-03-11 21:23:09 +0530

    calltoaction

--- src/components/sections/callToAction.jsx
+++ src/components/sections/callToAction.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import Link from 'next/link'
-import { RiDownloadLine } from '@remixicon/react'
+import { RiWhatsappLine } from '@remixicon/react'
 import SlideUp from '@/utlits/animations/slideUp'
 
 const CallToAction = () => {
@@ -8,19 +7,37 @@ const CallToAction = () => {
         <section className="call-to-action-area">
             <div className="container">
                 <div className="row">
-                    {/* <!-- START ABOUT TEXT DESIGN AREA --> */}
+
                     <div className="col-lg-12">
                         <SlideUp>
                             <div className="about-content-part call-to-action-part text-center">
-                                <h2>Are You Ready to kickstart your project with a touch of magic?</h2>
-                                <p>Reach out and let's make it happen ✨. I'm also available for full-time or Part-time opportunities to push the boundaries of design and deliver exceptional work.</p>
+
+                                <h2>
+                                    Ready to bring your ideas to life through video?
+                                </h2>
+
+                                <p>
+                                    Let’s create engaging visual content together.
+                                    I’m available for freelance projects, collaborations,
+                                    and full-time opportunities in video editing,
+                                    content creation, and media production.
+                                </p>
+
                                 <div className="hero-btns">
-                                    <Link href="/contact" className="theme-btn">Let's Talk  <i><RiDownloadLine size={16} /></i></Link>
+                                    <a
+                                        href="https://wa.me/918075289709"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        className="theme-btn"
+                                    >
+                                        Let's Talk <i><RiWhatsappLine size={16} /></i>
+                                    </a>
                                 </div>
+
                             </div>
                         </SlideUp>
                     </div>
-                    {/* <!-- / END ABOUT TEXT DESIGN AREA --> */}
+
                 </div>
             </div>
         </section>
