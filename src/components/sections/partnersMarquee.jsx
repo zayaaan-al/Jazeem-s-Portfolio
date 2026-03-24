2e22ead080ffe5b04596a64e6dbbd754836aa8f1
Al Zayan P <alzayanpoothakkal1156@gmail.com>
2026-03-24 22:06:11 +0530

    client links

--- src/components/sections/partnersMarquee.jsx
+++ src/components/sections/partnersMarquee.jsx
@@ -12,29 +12,35 @@ const PartnersMarquee = () => {
 
                         <Marquee>
 
-                            <a href="https://youtube.com/@channel1" target="_blank" rel="noopener noreferrer">
-                                <img src="/images/client-logos/slide 1.jpeg" alt="Client 1" />
+                            <a href="https://www.youtube.com/@ajmalkhanonline" target="_blank" rel="noopener noreferrer">
+                                <img src="/images/client-logos/slide 6.jpeg" alt="Client 6" />
                             </a>
 
-                            <a href="https://youtube.com/@channel2" target="_blank" rel="noopener noreferrer">
-                                <img src="/images/client-logos/slide 2.jpeg" alt="Client 2" />
-                            </a>
-
-                            <a href="https://youtube.com/@channel3" target="_blank" rel="noopener noreferrer">
-                                <img src="/images/client-logos/slide 3.jpeg" alt="Client 3" />
-                            </a>
-
-                            <a href="https://youtube.com/@channel4" target="_blank" rel="noopener noreferrer">
-                                <img src="/images/client-logos/slide 4.jpeg" alt="Client 4" />
-                            </a>
-
-                            <a href="https://youtube.com/@channel5" target="_blank" rel="noopener noreferrer">
+                            <a href="https://www.youtube.com/@Nihalkkaaaa" target="_blank" rel="noopener noreferrer">
                                 <img src="/images/client-logos/slide 5.jpeg" alt="Client 5" />
                             </a>
 
-                            <a href="https://youtube.com/@channel6" target="_blank" rel="noopener noreferrer">
-                                <img src="/images/client-logos/slide 6.jpeg" alt="Client 6" />
+                            <a href="https://www.youtube.com/@Supervaava" target="_blank" rel="noopener noreferrer">
+                                <img src="/images/client-logos/slide 1.jpeg" alt="Client 1" />
                             </a>
+
+                             <a href="https://www.youtube.com/@backpackerarunima2466" target="_blank" rel="noopener noreferrer">
+                                <img src="/images/client-logos/slide 3.jpeg" alt="Client 3" />
+                            </a>
+
+                            <a href="https://www.youtube.com/@basilvlogss" target="_blank" rel="noopener noreferrer">
+                                <img src="/images/client-logos/slide 2.jpeg" alt="Client 2" />
+                            </a>
+
+                           
+
+                            <a href="https://www.youtube.com/@arxhuvlogs" target="_blank" rel="noopener noreferrer">
+                                <img src="/images/client-logos/slide 4.jpeg" alt="Client 4" />
+                            </a>
+
+                            
+
+                            
 
                         </Marquee>
 
